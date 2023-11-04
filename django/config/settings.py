c1925900ee59160eafe6318a324ed216a18c6e1c
Akani Mashaba <akanimmashaba@gmail.com>
2023-11-04 21:15:31 +0200

    added requiremnts.txt and some config changes

--- django/config/settings.py
+++ django/config/settings.py
@@ -11,26 +11,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_2-+r2-*tp&qex*opmk78m8fdaly*@$w^+o#5*3ufsd)6kgy5d'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['akanimashaba.co.za','www.akanimashaba.co.za']
 
 
 # Application definition
@@ -138,3 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
           
 
+cloudinary.config( 
+  cloud_name = "", 
+  api_key = "", 
+  api_secret = "" 
+)
